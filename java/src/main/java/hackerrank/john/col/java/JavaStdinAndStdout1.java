a8d154267c508a43ef920e3ff8c9a4a6b24581ed
John <john.19col@gmail.com>
2019-10-27 13:44:53 -0500

    Add 3 new java introduction challengesx

---
+++ java/src/main/java/hackerrank/john/col/java/JavaStdinAndStdout1.java
@@ -0,0 +1,17 @@
+package hackerrank.john.col.java;
+
+import java.util.*;
+
+public class JavaStdinAndStdout1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
